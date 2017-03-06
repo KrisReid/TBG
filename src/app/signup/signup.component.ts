debd29a1bf63ad8a881ef75b2b96dbd7d5490f6a
Kris <krisreid_06@hotmail.com>
2017-03-06 21:05:13 +0000

    validators added w. bug
    validators added - with a bug

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -27,39 +27,84 @@ export class SignupComponent implements OnInit {
   submitted = false;
 
   addPlayerForm: FormGroup;
-  fullName = new FormControl('', Validators.required);
-  email = new FormControl('', Validators.required);
-  password = new FormControl('', Validators.required);
-  // debt = new FormControl('', Validators.required);
-  terms = new FormControl('', Validators.required);
+  fullName = new FormControl();
+  email = new FormControl();
+  password = new FormControl();
+  terms = new FormControl();
 
   constructor(
-    // private http: Http,
     private router: Router,
     private formBuilder: FormBuilder,
     private playerService: PlayerService) { }
 
   ngOnInit() {
+
     this.addPlayerForm = this.formBuilder.group({
-      fullName: this.fullName,
-      email: this.email,
-      password: this.password,
-      terms: this.terms,
-      debt: 0
+      fullName: [this.fullName,
+        [Validators.required, Validators.pattern('[A-Z a-z]*')]],
+      email: [this.email,
+        [Validators.required, Validators.pattern("^[A-Za-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$")]],
+      password: [this.password,
+        [Validators.required, Validators.pattern("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$")]],
+      terms: [this.terms,
+        [Validators.required, this.mustBeChecked]],
+      debt: 0,
     });
 
-    // this.signupForm = this.builder.group({
-    //   fullName: [this.entry.fullName,
-    //     [Validators.required, Validators.pattern('[A-Z a-z]*')]],
-    //   email: [this.entry.email,
-    //     [Validators.required, Validators.pattern("^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$")]],
-    //   password: [this.entry.password,
-    //     [Validators.required, Validators.minLength(3)]],
-    //   terms: [this.entry.terms,
-    //     [this.mustBeChecked]],
-    //   debt: [this.entry.debt, []]
-    // });
+    // // This clears the form so its not populated on load, but also clears the 0 value of debt
+    // console.log('Form reset happens');
+    // this.addPlayerForm.reset();
+
   }
+
+  mustBeChecked(control: AbstractControl): {[key: string]: string} {
+    if (!control.value) {
+      return {mustBeCheckedError: 'Must be checked'};
+    } else {
+      return null;
+    }
+  }
+
+  isInvalid(controlName: string){
+    return this.addPlayerForm.controls[controlName].invalid
+  }
+
+  getValidationMessage(controlName: string) {
+    let message = '';
+    let control = this.addPlayerForm.get(controlName);
+    if (control) {
+      console.log(control);
+      let messages = this.validationMessages[controlName];
+      if (messages && control.errors) {
+        for (const key in control.errors) {
+          message += messages[key] + ' ';
+        }
+      }
+    }
+    return message == ''
+      ? 'Control value is invalid.'
+      : message;
+  }
+
+  validationMessages = {
+    fullName: {
+      required: 'Please specify your full name',
+      pattern: 'Please ensure there are only letters in here'
+    },
+    email: {
+      required: 'Please specify your email address.',
+      pattern: 'please ensure an actual email address is entered'
+    },
+    password: {
+      required: 'Please specify your password.',
+      pattern: 'Password contain at least one cpaital letter, one number and be greater than 6 characters long.'
+    },
+    terms: {
+      required: 'Please accept the terms'
+    }
+  };
+
+
 
   SignUp() {
     console.log(this.addPlayerForm.value);
