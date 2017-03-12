c0f31736a427513e6791043a091194499863a1bb
Kris <krisreid_06@hotmail.com>
2017-03-12 22:23:13 +0000

    A
    A

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -10,11 +10,36 @@ import {GameService} from '../services/game.service';
 export class PlayComponent {
 
   playerId = '58ab2c99a757ea16ac3eb0f9';
-  nextGame = '08/08/2017'
+
+  games = [];
 
   today = Date.now();
 
   constructor(
-    private router: Router) { }
+    private router: Router,
+    private gameService: GameService) { }
+
+  ngOnInit() {
+    this.getGames();
+  }
+
+  getGames() {
+    this.gameService.getGames().subscribe(
+      data => this.games = data,
+      error => console.log(error)
+    );
+  }
+
+  identifyNextGame() {
+    console.log(this.today);
+    for (let game of this.games) {
+      if (game.date > '2017-03-16'){
+        return game
+      }
+      //IF the date is greater than today
+      //THEN show the array in order of the games available
+      console.log(game);
+    }
+  }
 
 }
