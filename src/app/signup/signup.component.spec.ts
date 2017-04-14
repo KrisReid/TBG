bec1ec8ad192644d3bf606f461a0c33b850ee4b9
Kris <krisreid_06@hotmail.com>
2017-04-14 15:45:53 +0100

    Code Refactor and many many bug fixes

---
+++ src/app/signup/signup.component.spec.ts
@@ -0,0 +1,9 @@
+// import { TestBed, async } from '@angular/core/testing';
+//
+// import { SignupComponent } from './signup.component';
+//
+// describe('my first test',() => {
+//   it('my first expectation',() => {
+//     expect(true).toBeTruthy()
+//   })
+// })
