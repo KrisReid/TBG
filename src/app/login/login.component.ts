9587956e794eb1a2998890e8a32fb09f2d6392ab
Kris <krisreid_06@hotmail.com>
2017-04-16 10:46:11 +0100

    Guarding Phase 1
    Have added:
    - Welcome Module to route all users to
    - guard file
    - mock user auth service
    
    This along with some modifications will not allow a user to view any
    page until they select a button of log in as ‘user’ or ‘admin’ (both
    allow access to everything at the moment.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 
+import {UserAuthService} from '../services/user-auth.service';
+
 @Component({
   selector: 'login',
   templateUrl: './login.template.html',
@@ -9,11 +11,28 @@ import {Router} from '@angular/router';
 export class LoginComponent {
 
   constructor(
-    private router: Router) { }
+    private router: Router,
+    public authService: UserAuthService
+  ) { }
 
   Signup() {
     console.log('Signup button hit')
     this.router.navigate(['/signup']);
+  }
+
+  login(userName: string) {
+    this.authService.login(userName).then(() => {
+      let redirectUrl = this.authService.redirectUrl
+        ? this.authService.redirectUrl
+        : '/';
+      this.router.navigate([redirectUrl]);
+    })
+  }
+
+  logout() {
+    this.authService.logout().then(() => {
+      //
+    }) 
   }
 
 }
